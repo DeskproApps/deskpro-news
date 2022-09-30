5b81fc867244cebf83d8a4d1984b6bf884c525c8
Jake Biddell <jake.biddell@gdeskpro.co.uk>
2022-09-30 12:26:33 +0100

    fix lint warning

--- src/components/FeedItem/NewsFeedItem.tsx
+++ src/components/FeedItem/NewsFeedItem.tsx
@@ -1,32 +1,51 @@
 import { FeedItem } from "../../types";
-import { HorizontalDivider, useDeskproAppTheme } from "@deskpro/app-sdk";
+import { useDeskproAppTheme } from "@deskpro/app-sdk";
 import "./NewsFeedItem.css";
 
 type NewsFeedItemProps = {
-    item: FeedItem;
-    locale: string;
+  item: FeedItem;
+  locale: string;
 };
 
 export const NewsFeedItem = ({ item, locale }: NewsFeedItemProps) => {
-    const { theme } = useDeskproAppTheme();
+  const { theme } = useDeskproAppTheme();
 
-    const published = new Date(item.published);
+  const published = new Date(item.published);
 
-    const dateFormat: Intl.DateTimeFormatOptions = {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-    };
+  const dateFormat: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
 
-    return (
-        <div className="news-feed-item" style={{ borderColor: theme.colors.grey20 }}>
-            <h2 className="news-feed-item-title" style={{ color: theme.colors.grey100 }}>
-                <a href={item.link} target="_blank" style={{ color: theme.colors.grey100 }}>{item.title}</a>
-            </h2>
-            <time className="news-feed-item-date" style={{ color: theme.colors.grey80 }} dateTime={published.toISOString()}>
-                {published.toLocaleDateString(locale, dateFormat)}
-            </time>
-            <div className="news-feed-item-body" dangerouslySetInnerHTML={{ __html: item.description }} />
-        </div>
-    );
+  return (
+    <div
+      className="news-feed-item"
+      style={{ borderColor: theme.colors.grey20 }}
+    >
+      <h2
+        className="news-feed-item-title"
+        style={{ color: theme.colors.grey100 }}
+      >
+        <a
+          href={item.link}
+          target="_blank"
+          style={{ color: theme.colors.grey100 }}
+        >
+          {item.title}
+        </a>
+      </h2>
+      <time
+        className="news-feed-item-date"
+        style={{ color: theme.colors.grey80 }}
+        dateTime={published.toISOString()}
+      >
+        {published.toLocaleDateString(locale, dateFormat)}
+      </time>
+      <div
+        className="news-feed-item-body"
+        dangerouslySetInnerHTML={{ __html: item.description }}
+      />
+    </div>
+  );
 };
