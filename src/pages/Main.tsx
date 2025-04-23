ee84a1dccc4d0ee323705e01b02843ce583123e1
OJ Abba <phavorabba@gmail.com>
2025-04-23 09:54:13 +0100

    [Chore] Remove console log

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -128,7 +128,6 @@ export const Main = () => {
       onShow: (context) => {
         setIsShown(true);
         getFeed(context);
-        console.log(context.data.env.release)
       },
     },
     [client]
