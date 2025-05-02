ca42ee9493012e62cf48c92501da08209acaac7d
OJ Abba <phavorabba@gmail.com>
2025-05-02 19:03:22 +0100

    [Chore] Introduce react router

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,25 @@ import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "flatpickr/dist/themes/light.css";
 import "simplebar/dist/simplebar.min.css";
 import "tippy.js/dist/tippy.css";
-import { DeskproAppProvider } from "@deskpro/app-sdk";
+import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import ReleaseAndNewsFeedView from "./components/ReleaseAndNewsFeedView";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-      <DeskproAppProvider>
-        <ReleaseAndNewsFeedView />
-      </DeskproAppProvider>
+    <DeskproAppProvider>
+      <HashRouter>
+        <Suspense fallback={<LoadingSpinner />}>
+          <ErrorBoundary fallback={<>An unexpected error occurred</>}>
+            <Routes>
+              <Route index element={<ReleaseAndNewsFeedView />} />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+      </HashRouter>
+    </DeskproAppProvider>
   );
 }
 
--- src/components/ReleaseAndNewsFeedView/ReleaseAndNewsFeedView.tsx
+++ src/components/ReleaseAndNewsFeedView/ReleaseAndNewsFeedView.tsx
@@ -34,6 +34,7 @@ export default function ReleaseAndNewsFeedView() {
     });
 
     client.setTitle("What's New?");
+    client.focus()
   });
 
   function handleAllItemsSeen() {
